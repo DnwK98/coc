3aada7817317d914f1c25da52c18c38836908d02
Damian Wójcik <damian022w@wp.pl>
2018-11-07 16:00:34 +0100

    Add Main page content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,119 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="main_image">
+        <div class="dimness">
+            <div class="container text-center main_image_content">
+                {{__('Hello.work')}}
+            </div>
+        </div>
+    </div>
 
-    <div style="padding: 25px; font-size: 26px;">
-        Welcome in COC Application
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-6 col-md-12 part hidden-sm">
+                <img class="img-fluid"
+                     src="https://as2.ftcdn.net/jpg/00/51/23/87/500_F_51238780_PqgPREAjFABzy8W8v0OGKmIZvbQ6iHpx.jpg">
+            </div>
+
+            <div class="col-lg-1 col-md-12 part">
+            </div>
+
+            <div class="col-lg-5 col-md-12 part">
+                <br/><br/>
+                Kontroluj swój komputer gdzie tylko jesteś na całym świecie za pomocą prostego web api. <br/><br/>
+                Bez zbędnych programów, wystarczy tylko przeglądarka.<br/><br/>
+                I to wszystko ZA DARMO
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-lg-4 col-md-12 part hidden-sm">
+                <img class="img-fluid"
+                     src="https://as1.ftcdn.net/jpg/01/05/92/34/500_F_105923482_qafGtTFNjdfO27HTi979ND3VaATGdphJ.jpg">
+            </div>
+
+            <div class="col-lg-8 part" style="font-size: 22px;">
+                <div class="col-lg-12 main_color_text text-center" style="font-size: 40px;">
+                    Projekt Open-Source
+                </div>
+
+                Używając aplikacji Open-source wspierasz niezależnych twórców.<br/>
+                Ponadto możesz być bezpieczny o swój komputer dzięki możliwości sprawdzenia co dokładnie dane
+                oprogramowanie robi.
+                <br/>
+
+            </div>
+
+
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 col-md-12 part">
+                <br/>
+                Kontroluj swój komputer gdzie tylko jesteś na całym świecie za pomocą prostego web api. <br/><br/>
+                Bez zbędnych programów, wystarczy tylko przeglądarka.
+            </div>
+            <div class="col-lg-6 col-md-12 part hidden-sm">
+                <img class="img-fluid"
+                     src="https://as1.ftcdn.net/jpg/00/85/18/36/500_F_85183610_bYjEh4tfTB2RFwje1AZQGqk5j2M5szPK.jpg">
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-lg-4 part">
+                <img class="img-fluid "
+                     src="https://as1.ftcdn.net/jpg/00/71/54/48/500_F_71544866_GRj4S5OTLnhAwznIiwDTFq0HzyEzDOpJ.jpg">
+                <br/>
+                <div style="font-size: 18px; padding:10px;">
+                    Restart Your PC wherever you are
+                </div>
+            </div>
+
+            <div class="col-lg-4 part">
+                <img class="img-fluid"
+                     src="https://as1.ftcdn.net/jpg/00/80/89/44/500_F_80894400_aXgrOSbQJwoWjq2lPYNMa7ctSviAmxcC.jpg">
+                <br/>
+                <div style="font-size: 18px; padding:10px;">
+                    Restart Your PC wherever you are
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 part">
+                <img class="img-fluid"
+                     src="https://as2.ftcdn.net/jpg/01/05/91/33/500_F_105913387_bSNE7NXxLJriyyaR7kbvyP6V7fC7BPEd.jpg">
+                <br/>
+                <div style="font-size: 18px; padding:10px;">
+                    Restart Your PC wherever you are
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-lg-9 part">
+
+                <div class="col-lg-12 main_color_text" style="font-size: 45px;">
+                    Projekt Open-Source
+                </div>
+                Używając aplikacji Open-source wspierasz niezależnych twórców.<br/>
+                Ponadto możesz być bezpieczny o swój komputer dzięki możliwości sprawdzenia co dokładnie dane
+                oprogramowanie robi.
+                <br/>
+
+            </div>
+
+            <div class="col-lg-3 col-md-12 part hidden-sm">
+                <img class="img-fluid"
+                     src="https://as1.ftcdn.net/jpg/00/83/05/00/500_F_83050019_PNgUwzBXAsHSORc52wdROmSWNeGEpIh5.jpg">
+            </div>
+
+        </div>
+
     </div>
 
 @endsection
